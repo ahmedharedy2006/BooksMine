9296afd1056563761e0df5272434c593cc005a6e
ahassan200695 <ahassan200695@gmail.com>
2025-01-30 22:37:08 +0200

    feat: implement Login GET and POST methods in AccountController
    - Added `Login` GET method to display the login form.
    - Implemented `Login` POST method to handle user authentication.
    - Integrated `LoginViewModel` for data binding and validation.
    - Added error handling for invalid login attempts.

--- BooksMineWeb/Areas/Customer/Controllers/AccountController.cs
+++ BooksMineWeb/Areas/Customer/Controllers/AccountController.cs
@@ -66,5 +66,41 @@ namespace BooksMineWeb.Areas.Customer.Controllers
             return View(model);
         }
 
+
+        [AllowAnonymous]
+        public IActionResult Login(string returnurl = null)
+        {
+            ViewData["ReturnUrl"] = returnurl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model, string returnurl = null)
+        {
+            ViewData["ReturnUrl"] = returnurl;
+            returnurl = returnurl ?? Url.Content("~/");
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe,
+                    lockoutOnFailure: true);
+                if (result.Succeeded)
+                {
+                    var user = await _userManager.GetUserAsync(User);
+                   
+                    return LocalRedirect(returnurl);
+                }
+           
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return View(model);
+                }
+            }
+            return View(model);
+        }
+
+
     }
 }
