4c16537210d0b37fbf8b5d158846808b7d89b6d7
ahassan200695 <ahassan200695@gmail.com>
2025-01-30 22:33:51 +0200

    feat: add standard definitions in SD class
    - Added standard definitions and constants to the `SD` class for uniformity.
    - Included commonly used values and settings for easier reference throughout the application.
    - Ensured that the SD class serves as a centralized source for standard definitions.

--- BooksMine.Utility/SD.cs
+++ BooksMine.Utility/SD.cs
@@ -2,6 +2,9 @@
 {
     public class SD
     {
+        public const string User = "User";
+        public const string Admin = "Admin";
+        public const string SuperAdmin = "SuperAdmin";
 
     }
 }
