fcc288a2ad9917dfa5cc8b54596e346ccb83613a
ahassan200695 <ahassan200695@gmail.com>
2025-01-30 22:31:30 +0200

    feat: create Account controller with Register GET and POST methods
    - Created `AccountController` for user-related functions.
    - Implemented `Register` GET method to display the registration form.
    - Implemented `Register` POST method to handle user registration and validation.
    - Integrated `RegisterViewModel` for data binding and input validation.

---
+++ BooksMineWeb/Areas/Customer/Controllers/AccountController.cs
@@ -0,0 +1,70 @@
+﻿using BooksMine.Models.Models;
+using BooksMine.Models.ViewModels;
+using BooksMine.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BooksMineWeb.Areas.Customer.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager , RoleManager<IdentityRole> roleManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(string returnurl = null)
+        {
+            if (!_roleManager.RoleExistsAsync(SD.Admin).GetAwaiter().GetResult())
+            {
+                await _roleManager.CreateAsync(new IdentityRole(SD.Admin));
+                await _roleManager.CreateAsync(new IdentityRole(SD.User));
+            }
+
+
+            ViewData["ReturnUrl"] = returnurl;
+            
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model, string returnurl = null)
+        {
+            ViewData["ReturnUrl"] = returnurl;
+            returnurl = returnurl ?? Url.Content("~/");
+            if (ModelState.IsValid)
+            {
+                var user = new AppUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                   
+                    await _userManager.AddToRoleAsync(user, SD.User);
+                    
+                    await _signInManager.SignInAsync(user, isPersistent: true);
+                    return LocalRedirect(returnurl);
+                }
+            }
+            return View(model);
+        }
+
+    }
+}
