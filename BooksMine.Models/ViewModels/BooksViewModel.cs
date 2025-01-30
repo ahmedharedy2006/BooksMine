f0a55ab4937f6c88e402c28163cc9aaa2d7fcc6e
ahassan200695 <ahassan200695@gmail.com>
2025-01-30 18:15:04 +0200

    feat: add BookViewModel
    - Created BookViewModel to structure book-related data.
    - Added properties for handling book information in views.
    - Improved separation of concerns between models and views.

---
+++ BooksMine.Models/ViewModels/BooksViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksMine.Models.ViewModels
+{
+    public class BooksViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+
+        public int NoInStock { get; set; }
+
+        public string PublisherName { get; set; }
+
+        public string AuthorName { get; set; }
+
+        public string CategoryName { get; set; }
+    }
+}
