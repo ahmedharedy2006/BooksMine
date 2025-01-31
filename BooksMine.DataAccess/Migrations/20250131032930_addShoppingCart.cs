0f3ff1fd63b34627c585720f15679f7a319c5998
ahassan200695 <ahassan200695@gmail.com>
2025-01-31 20:36:04 +0200

    chore: add and apply migrations for ShoppingCart model
    - Generated a new migration to incorporate the `ShoppingCart` model into the database schema.
    - Applied migrations to update the database structure accordingly.
    - Ensured schema consistency for shopping cart functionality.

---
+++ BooksMine.DataAccess/Migrations/20250131032930_addShoppingCart.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BooksMine.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addShoppingCart : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "shoppingCarts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AppUserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    bookId = table.Column<int>(type: "int", nullable: false),
+                    Count = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_shoppingCarts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_shoppingCarts_AspNetUsers_AppUserId",
+                        column: x => x.AppUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_shoppingCarts_Books_bookId",
+                        column: x => x.bookId,
+                        principalTable: "Books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_shoppingCarts_AppUserId",
+                table: "shoppingCarts",
+                column: "AppUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_shoppingCarts_bookId",
+                table: "shoppingCarts",
+                column: "bookId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "shoppingCarts");
+        }
+    }
+}
