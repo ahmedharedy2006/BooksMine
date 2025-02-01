062c63792786fb61cd77861c70776780e1518e02
ahassan200695 <ahassan200695@gmail.com>
2025-02-01 21:49:13 +0200

    feat: add order and payment status constants in SD class - Introduced constants for various order statuses: `Pending`, `Approved`, `Processing`, `Shipped`, `Cancelled`, `Refunded`. - Added payment status constants: `Pending`, `Approved`, `ApprovedForDelayedPayment`, `Rejected`. - Centralized status management for easier updates and consistency across the application.

--- BooksMine.Utility/SD.cs
+++ BooksMine.Utility/SD.cs
@@ -6,5 +6,16 @@
         public const string Admin = "Admin";
         public const string SuperAdmin = "SuperAdmin";
 
+        public const string StatusPending = "Pending";
+        public const string StatusApproved = "Approved";
+        public const string StatusInProccess = "Proccessing";
+        public const string StatusShipped = "Shipped";
+        public const string StatusCancelled = "Cancelled";
+        public const string StatusRefunded = "Refunded";
+
+        public const string PaymentStatusPending = "Pending";
+        public const string PaymentStatusApproved = "Approved";
+        public const string PaymentStatusDelayedPayment = "ApprovedForDelayedPayment";
+        public const string PaymentStatusRejected = "Rejected";
     }
 }
